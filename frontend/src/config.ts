75fe58be47b75f97ddc5e8e9b23d2b47651f12d8
Alexander Zorkin <somnoynadno@yandex.ru>
2021-09-06 11:05:03 +0300

    .env removed from .dockerignore

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const wsAddress = (process.env.NODE_ENV === "production" ? process.env.REACT_APP_WS_ADDRESS : "ws://localhost:8000");
-export const appAddress = (process.env.NODE_ENV === "production" ? process.env.REACT_APP_ADDRESS : "http://localhost:3000");
+export const wsAddress = process.env.REACT_APP_WS_ADDRESS || "ws://localhost:8000";
+export const appAddress = process.env.REACT_APP_ADDRESS || "http://localhost:3000";
